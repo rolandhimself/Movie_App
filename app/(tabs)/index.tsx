d210e6d68ee7fc28954499d65d0f13e41e2f637a
rolandhimself <romappiah@st.ug.edu.gh>
2025-08-22 23:42:13 +0000

    Homepage SearchBar functionality

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,19 @@
-import { Text, View } from "react-native";
+import SearchBar from "@/components/SearchBar";
+import { icons } from "@/constants/icons";
+import { images } from '@/constants/images';
+import { Image, ScrollView, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-2xl text-dark-200">WELCOME!</Text>
+    <View className="flex-1 bg-primary">
+        <Image source={images.bg} className="absolute w-full z-0"/>
+        <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{minHeight:"100%",paddingBottom: 10}}>
+            <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"/>
+
+            <View className="fles-1 mt-5">
+                <SearchBar/>
+            </View>
+        </ScrollView>
     </View>
   );
 }
