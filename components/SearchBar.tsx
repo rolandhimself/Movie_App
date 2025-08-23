a7692726b73ddbba0d45ecbeae2ffd436be8d8c3
rolandhimself <romappiah@st.ug.edu.gh>
2025-08-23 00:00:52 +0000

    Search Bar routing and hook implemented

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -2,14 +2,18 @@ import { icons } from '@/constants/icons';
 import React from 'react';
 import { Image, TextInput, View } from 'react-native';
 
+interface Props {
+    placeholder: string;
+    onPress?: () => void;
+}
 
-const SearchBar = () => {
+const SearchBar = ({placeholder, onPress}:Props) => {
   return (
     <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
       <Image source={icons.search} className="size-5" resizeMode="contain" tintColor="#ab8bff"/>
       <TextInput
-        onPress={() => {}}
-        placeholder="Search"
+        onPress={onPress}
+        placeholder={placeholder}
         value=""
         placeholderTextColor="#a8b5db"
         onChangeText={() => {}}
