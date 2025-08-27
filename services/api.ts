7a8cf5aa2f4fb993c3a1628c408f3801aab41c58
rolandhimself <romappiah@st.ug.edu.gh>
2025-08-27 23:53:19 +0000

    API endpoints added

--- services/api.ts
+++ services/api.ts
@@ -1,15 +1,28 @@
+export const TMDB_CONFIG = {
+    BASE_URL: 'https://api.themoviedb.org/3',
+    API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
+    headers: {
+        accept: 'applicattion/json',
+        Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`
+    }
+}
 
+export const fetchMovies = async({query} : {query: string}) => {
+    const endpoint = query
+        ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+        : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+    const response = await fetch(endpoint, {
+        method: 'GET',
+        headers: TMDB_CONFIG.headers,
+    });
+
+    if (!response.ok) {
+        //@ts-ignore
+        throw new Error('Failed to fetch movies', response.statusText)
+    }
+
+    const data = await response.json();
+}
 
 // const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
-// const options = {
-//   method: 'GET',
-//   headers: {
-//     accept: 'application/json',
-//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYzljOWY3OTg0OGZmODAzMmQ5ZjhkZWMxNDdmM2VmOSIsIm5iZiI6MTc1NjE2NzIyNi43Mjk5OTk4LCJzdWIiOiI2OGFjZmMzYTljNGI4YTZiOGU2NzBmNTQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.kfb67W76SF1iyl9kAgqMzvwa-UClgZ581nzJZHxNnRk'
-//   }
-// };
-
-// fetch(url, options)
-//   .then(res => res.json())
-//   .then(json => console.log(json))
-//   .catch(err => console.error(err));
