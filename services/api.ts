aae944e0b84f9a1b8fb1543e843fbb0d96468902
rolandhimself <romappiah@st.ug.edu.gh>
2025-08-27 23:52:54 +0000

    API endpoints implemented

---
+++ services/api.ts
@@ -0,0 +1,15 @@
+
+
+// const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYzljOWY3OTg0OGZmODAzMmQ5ZjhkZWMxNDdmM2VmOSIsIm5iZiI6MTc1NjE2NzIyNi43Mjk5OTk4LCJzdWIiOiI2OGFjZmMzYTljNGI4YTZiOGU2NzBmNTQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.kfb67W76SF1iyl9kAgqMzvwa-UClgZ581nzJZHxNnRk'
+//   }
+// };
+
+// fetch(url, options)
+//   .then(res => res.json())
+//   .then(json => console.log(json))
+//   .catch(err => console.error(err));
