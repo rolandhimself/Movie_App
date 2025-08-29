559edbf0440cfff3f41341f8db3e34c20b68fee7
rolandhimself <romappiah@st.ug.edu.gh>
2025-08-29 15:18:28 +0000

    Commenting out

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,6 +1,7 @@
 import { icons } from '@/constants/icons';
 import React from 'react';
 import { Image, TextInput, View } from 'react-native';
+//importing from react native
 
 interface Props {
     placeholder: string;
@@ -10,7 +11,9 @@ interface Props {
 const SearchBar = ({placeholder, onPress}:Props) => {
   return (
     <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
+        //flex-row means everything inside the view will be in a row horizontally items-center means everything will be aligned in the center bg-dark-200 is the background color rounded-full is to make the corners rounded px-5 is padding on x axis py-4 is padding on y axis
       <Image source={icons.search} className="size-5" resizeMode="contain" tintColor="#ab8bff"/>
+        //size-5 is width and height resizeMode is to contain the image within the view tintColor is to change the color of the image
       <TextInput
         onPress={onPress}
         placeholder={placeholder}
@@ -18,6 +21,7 @@ const SearchBar = ({placeholder, onPress}:Props) => {
         placeholderTextColor="#a8b5db"
         onChangeText={() => {}}
         className="flex-1 ml-2 text-white"
+        //flex-1 is to take up all the available space ml-2 is margin on left text-white is to make the text color white
       ></TextInput>
     </View>
   )
