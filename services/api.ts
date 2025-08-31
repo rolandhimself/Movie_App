05d433d388771c128cd06b3bf1523c89a856a808
rolandhimself <romappiah@st.ug.edu.gh>
2025-08-31 01:17:48 +0000

    comments

--- services/api.ts
+++ services/api.ts
@@ -2,8 +2,8 @@ export const TMDB_CONFIG = {
     BASE_URL: 'https://api.themoviedb.org/3',
     API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
     headers: {
-        accept: 'applicattion/json',
-        Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`
+        accept: 'application/json', //sets what type of response we want (JSON and not XML or HTML
+        Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}` //authenticates API requests
     }
 }
 
