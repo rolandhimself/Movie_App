64fdae8cdbe08c8c53b8ba16fa8b17625f43fc66
rolandhimself <romappiah@st.ug.edu.gh>
2025-08-31 14:43:28 +0000

    Fetching and displaying movies

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,21 +1,35 @@
 import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from '@/constants/images';
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
 import { useRouter } from "expo-router";
-import { Image, ScrollView, View } from "react-native";
+import { ActivityIndicator, Image, ScrollView, Text, View } from "react-native";
+
 
 export default function Index() {
   const router = useRouter();
 
+  const { 
+    data:movies ,
+    loading: moviesLoading, 
+    error: moviesError} = useFetch(()=> fetchMovies({query:''})); //fetching movies with empty query, empty query still works because API has been set to provide popular movies when there's no query
+
   return (
     <View className="flex-1 bg-primary">
         <Image source={images.bg} className="absolute w-full z-0"/>
-        <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{minHeight:"100%",paddingBottom: 10}}>
+        <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{minHeight:"100%",paddingBottom: 10}}> //px-5 is padding on x axis showsVerticalScrollIndicator=false is to hide the scroll bar
             <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"/>
-
-            <View className="fles-1 mt-5">
+            {moviesLoading ? (
+              <ActivityIndicator size="large" color="#0000ff" className="mt-10 self-center"/>
+            ): moviesError ? (
+              <Text>Error: {moviesError ?.message}</Text>
+            ):(
+               <View className="fles-1 mt-5">
                 <SearchBar onPress={() => router.push("/search")} placeholder="Search for a movie"/>
             </View>
+            )}
+           
         </ScrollView>
     </View>
   );
--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native'
 
 const saved = () => {
   return (
-    <View>
+    <View> //View is a container component in react native
       <Text>saved</Text>
     </View>
   )
