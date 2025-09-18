061cb94b1121f0477d0c2c6cb8f0e5ddb4bf7951
rolandhimself <romappiah@st.ug.edu.gh>
2025-09-18 18:12:27 +0000

    search page

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,11 +1,36 @@
+import MovieCard from '@/components/MovieCard';
 import { images } from '@/constants/images';
-import { Image, Text, View } from 'react-native';
+import { fetchMovies } from '@/services/api';
+import useFetch from '@/services/useFetch';
+import { useRouter } from 'expo-router';
+import { FlatList, Image, View } from 'react-native';
 
 const search = () => {
+  const router = useRouter();
+
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+  } = useFetch(() => fetchMovies({query: ''}))
+
   return (
     <View className="flex-1 bg-primary">
-      <Image source = {images.bg} className="absolute w-full z-0"/>
-      <Text>search</Text>
+      <Image source = {images.bg} className="absolute w-full z-0" resizeMode="cover"/>
+
+      <FlatList 
+      data={movies} 
+      renderItem={({item})=> <MovieCard {...item}/>}
+      keyExtractor={(item) => item.id.toString()}
+      numColumns={3}
+      columnWrapperStyle={
+       { justifyContent: 'flex-start',
+        gap: 16,
+        marginVertical: 16,
+        marginBottom: 10,}
+      }
+      className="px-5"
+      />
     </View>
   );
 }
