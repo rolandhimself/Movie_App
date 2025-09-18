e3e7f5ad5e5720e04242249b62fa9e27ffb6b5f5
rolandhimself <romappiah@st.ug.edu.gh>
2025-09-18 16:27:55 +0000

    Movie card done

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,5 +1,6 @@
+import { icons } from '@/constants/icons'
 import { Link } from 'expo-router'
-import { Image, Text, TouchableOpacity } from 'react-native'
+import { Image, Text, TouchableOpacity, View } from 'react-native'
 
 
 const MovieCard = ({id, poster_path, title, vote_average, release_date}: Movie) => {
@@ -14,10 +15,17 @@ const MovieCard = ({id, poster_path, title, vote_average, release_date}: Movie) 
             className="w-full h-52 rounded-lg"
             resizeMode="cover"
         />
-        <Text className="text-sm font-bold text-white">{title}</Text>
+        <Text className="text-sm font-bold text-white" numberOfLines={1}>{title}</Text>
 
         <View className="flex-row items-center justify-start gap-x-1">
-            <Image src={icons.star} class/>
+            <Image source ={icons.star} className="size-4"/>
+            <Text className="text-xs text-white font-bold uppercase">{(vote_average/2).toFixed(2)}</Text>
+        </View>
+        <View className="flex-row items-center justify-between">
+            <Text className = "text-xs text-light-300 font-medium mt-1">{release_date?.split('-')[0]}</Text>
+            {/*(?.)is used so if release_date is undefined it doesn't return an error
+            split('-') means split it into an array splitting using the -
+            [0] means return the first element of that array.*/}
         </View>
     </TouchableOpacity>
    </Link>
