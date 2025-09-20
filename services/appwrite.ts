fa729b46a7665753ac13c0d606486b9e852cbd89
rolandhimself <romappiah@st.ug.edu.gh>
2025-09-20 22:05:51 +0000

    appwrite setup

--- services/appwrite.ts
+++ services/appwrite.ts
@@ -1,3 +1,6 @@
-export const updateSearchCount = async (query: string)=>{
+const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
+const COLLECTION_ID = process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
 
+export const updateSearchCount = async (query: string, movie: Movie)=>{
+    const result = await
 }
