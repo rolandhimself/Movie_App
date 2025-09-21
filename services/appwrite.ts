104665beb3acd8f1f089f78bf1da6d9329508ec1
rolandhimself <romappiah@st.ug.edu.gh>
2025-09-21 21:58:55 +0000

    appwrite client

--- services/appwrite.ts
+++ services/appwrite.ts
@@ -1,5 +1,10 @@
+import {Client} from "react-native-appwrite";
+
 const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
 const COLLECTION_ID = process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
+
+const client = new Client()
+    .setEndpoint('https://cloud.appwrite.io/v1') // The API endpoint
 
 export const updateSearchCount = async (query: string, movie: Movie)=>{
     const result = await
