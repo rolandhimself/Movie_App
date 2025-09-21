2cf2ebcb0af7d7c1cf9b69f17af21ca14d518795
rolandhimself <romappiah@st.ug.edu.gh>
2025-09-21 22:16:02 +0000

    appwrite

--- services/appwrite.ts
+++ services/appwrite.ts
@@ -1,11 +1,21 @@
-import {Client} from "react-native-appwrite";
+import { Client, Databases, Query } from "react-native-appwrite";
 
 const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
 const COLLECTION_ID = process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
 
 const client = new Client()
     .setEndpoint('https://cloud.appwrite.io/v1') // The API endpoint
+    .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!)
+
+const database = new Databases(client);
 
 export const updateSearchCount = async (query: string, movie: Movie)=>{
-    const result = await
+    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, 
+       [ Query.equal('searchTerm', query)]
+    )
+
+    if (result.documents.length > 0){
+        const existingMovie = result.documents[0];
+    }
+
 }
